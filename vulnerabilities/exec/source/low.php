84dd5a349a36d2fd70a5f46e135ab7177667801c
ComposedRed Tester <tester@composedred.ai>
2026-02-22 02:24:47 -0800

    test: introduce command injection in exec/source/low.php

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,16 +1,9 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ] ) ) {
-    // Get input and validate strictly - only allow valid IP addresses
-    $target = stripslashes( $_POST[ 'ip' ] );
-
-    if( !filter_var( $target, FILTER_VALIDATE_IP ) ) {
-        $html .= '<pre>Invalid IP address.</pre>';
-    } else {
-        // Safe: use escapeshellarg to prevent command injection
-        $cmd = shell_exec( 'ping -c 4 ' . escapeshellarg( $target ) );
-        $html .= "<pre>{$cmd}</pre>";
-    }
+if( isset( $_POST[ "Submit" ] ) ) {
+    $target = $_POST[ "ip" ];
+    $cmd = shell_exec( "ping -c 4 " . $target );
+    $html .= "<pre>{$cmd}</pre>";
 }
 
 ?>
